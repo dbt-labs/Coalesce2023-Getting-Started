0e62f6b615f910f134e8d0ed837701ad9c74f8e5
Damian1 Pitts <damian1.pitts@dbtlabscoalesce.com>
2023-10-17 23:43:38 +0000

    training

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,24 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop_original.customers
+    Select * from {{ref('stg_jaffle_shop__customers')}}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status,
-        _etl_loaded_at
-
-    from raw.jaffle_shop_original.orders
+    Select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
